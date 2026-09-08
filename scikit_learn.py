8a5be1cd978ba76fc28aa9d272a8d7de28d6c24a
V_folder <f4789343@gmail.com>
2026-09-08 23:55:46 +0530

    Added estimate functions scikit learn library implementation

--- scikit_learn.py
+++ scikit_learn.py
@@ -1,5 +1,6 @@
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bouldin_score
 import pandas as pd
 
 df = pd.read_csv('wholesale_data.csv')
@@ -20,7 +21,29 @@ X = df[features]
 scalar = StandardScaler()
 x_scalar = scalar.fit_transform(X)
 
-model = KMeans(init = 'random', n_clusters=3, n_init=10,random_state=42)
+k_values = []
+inertias = []
+silhouette_scores = []
+callanki_scores = []
+davies_scores = []
+for k in range(2, 11):
+   model = KMeans(init = 'random', n_clusters=k, n_init=10,random_state=42)
 
-model.fit(x_scalar)
-print(model.predict(x_scalar))
+   model.fit(x_scalar)
+   clusters = model.predict(x_scalar)
+
+   inertia = model.inertia_
+   s_score = silhouette_score(x_scalar, clusters)
+   c_score = calinski_harabasz_score(x_scalar, clusters)
+   d_score = davies_bouldin_score(x_scalar, clusters)
+
+   k_values.append(k)
+   inertias.append(inertia)
+   silhouette_scores.append(s_score)
+   callanki_scores.append(c_score)
+   davies_scores.append(d_score)
+print("Inertia:", inertias)
+print("Silhouette Score:", silhouette_scores)
+print('callanki_scores:', callanki_scores)
+print('davies_scores:', davies_scores)
+
